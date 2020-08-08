bb66370a64e9ebd188c1b498bc45f9524076b3fb
HawkarShwany <hawkarshwany9@gmail.com>
2020-08-08 20:35:26 +0300

    added carousel slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'constants.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,109 +12,109 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Hawkar Shwany',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: kPrimaryColor,
+        accentColor: kButtonColor,
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomePageState extends State<HomePage> {
+  int _current = 0;
+  List<Widget> pages = [
+    Container(
+      height: double.infinity,
+      color: Colors.amber,
+      margin: EdgeInsets.all(40),
+      padding: EdgeInsets.all(20),
+      child: Text('data 1'),
+    ),
+    Container(
+      height: 500,
+      // color: Colors.blue,
+      padding: EdgeInsets.all(20),
+      child: Text('data 2'),
+    ),
+    Container(
+      height: 500,
+      // color: Colors.blue,
+      padding: EdgeInsets.all(20),
+      child: Text('data 2'),
+    ),
+    Container(
+      height: 500,
+      // color: Colors.blue,
+      padding: EdgeInsets.all(20),
+      child: Text('data 2'),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            child: CarouselSlider(
+              items: pages,
+              options: CarouselOptions(
+                scrollDirection: Axis.vertical,
+                enableInfiniteScroll: false,
+                // enlargeCenterPage: true,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                },
+                // enlargeStrategy: CenterPageEnlargeStrategy.height
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            // top: double.infinity/2,
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: AnimatedSmoothIndicator(
+                activeIndex: _current,
+                count: pages.length,
+                axisDirection: Axis.vertical,
+                effect: SwapEffect(
+                  activeDotColor: Color(0xffff8f00),
+                  dotColor: Color.fromRGBO(0, 0, 0, 0.4),
+                ),
+              ),
             ),
-          ],
-        ),
+            // child: Column(
+            //   children: pages.map((page) {
+            //     int index = pages.indexOf(page);
+            //     return Container(
+            //       width: 20,
+            //       height: 20,
+            //       decoration: BoxDecoration(
+            //       shape: BoxShape.circle,
+            //       color: _current == index
+            //         ? Color(0xffff8f00)
+            //         : Color.fromRGBO(0, 0, 0, 0.4),
+            //     ),
+            //       margin: EdgeInsets.all(10),
+            //     );
+            //   }).toList(),
+            // ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
